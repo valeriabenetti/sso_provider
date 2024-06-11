026092afa7a45b3c870048c39eeeb9ae1b6024b6
Val <vbenetti46@gmail.com>
2024-06-10 21:09:14 -0400

    Update Users to enable doorkeeper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :validatable, :confirmable
+
+  has_many :access_grants, class_name: 'Doorkeeper::AccessGrant', foreign_key: :resource_owner_id, dependent: :destroy
+  has_many :access_tokens, class_name: 'Doorkeeper::AccessToken', foreign_key: :resource_owner_id, dependent: :destroy
+
+  def generate_access_token
+    Doorkeeper::AccessToken.create!(resource_owner_id: id).token
+  end
+
+  def revoke_tokens!
+    access_tokens.update_all(revoked_at: Time.current)
+  end
 end
