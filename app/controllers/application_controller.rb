010d3d3d7a331247a0ef2b22d10c7d82818596ef
Val <vbenetti46@gmail.com>
2024-06-10 22:03:27 -0400

    Creating the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :check_session_validity
+
+  private
+
+  def check_session_validity
+    if current_user && current_user.invalidate_sessions_at.present? && current_user.invalidate_sessions_at > current_user.updated_at
+      sign_out current_user
+      redirect_to root_path, alert: 'Your session has expired. Please log in again.'
+    end
+  end
 end
